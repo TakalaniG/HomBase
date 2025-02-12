eed0bbd02e2084ddaa1223ccf8151f763d2edae0
TakalaniG <TakalaniG@TAKALANI-ASUS>
2025-02-12 19:24:51 +0200

    Add project files.

---
+++ WebApplication1/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿namespace HomBaseAPI.Data
+{
+    using Microsoft.EntityFrameworkCore;
+    using HomBaseAPI.Model;
+    using Microsoft.EntityFrameworkCore.Metadata.Internal;
+
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<HomBaseAPI.Model.Property> Properties { get; set; }
+        //public DbSet<User> Users { get; set; }
+    }
+}
---
+++ WebApplication1/Models/Property.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace HomBaseAPI.Model
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string Location { get; set; }
+        [Column(TypeName = "decimal(18,2)")] 
+        public decimal Price { get; set; }
+        public string ImageUrl { get; set; }
+    }
+}
